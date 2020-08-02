98688feb0eb2db8027a190ecbfe9075abdee1985
sarahmr63089 <sarahrose63089@gmail.com>
2020-08-02 14:04:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+let thankYous = []
+// let i
+
+function writeCards(array, event) {
+  for (let i = 0; i < array.length; i++) {
+    thankYous.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  }
+  return thankYous;
+}
+
+function countDown(num) {
+  while(num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
